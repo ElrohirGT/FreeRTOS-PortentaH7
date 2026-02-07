b39841219d249f73430a998f1a74e89737cd9d63
ElrohirGT <elrohirgt@gmail.com>
2026-02-07 16:00:13 -0600

    feat: fixed compilation

--- CM4/freertos.c
+++ CM4/freertos.c
@@ -1,41 +1,41 @@
 
-// This file is a part of MRNIU/FreeRTOS-PortentaH7 (https://github.com/MRNIU/FreeRTOS-PortentaH7).
+// This file is a part of MRNIU/FreeRTOS-PortentaH7
+// (https://github.com/MRNIU/FreeRTOS-PortentaH7).
 //
 // freertos.c for MRNIU/FreeRTOS-PortentaH7.
 
 #include "FreeRTOS.h"
-#include "task.h"
-#include "main.h"
 #include "cmsis_os.h"
+#include "main.h"
 #include "stm32h7xx_hal_gpio.h"
+#include "task.h"
 
 osThreadId_t cm4_task_handle;
-const osThreadAttr_t cm4_task_attributes = {
-  .name = "cm4_task",
-  .priority = (osPriority_t) osPriorityNormal,
-  .stack_size = 128 * 4
-};
+const osThreadAttr_t cm4_task_attributes = {.name = "cm4_task",
+                                            .priority =
+                                                (osPriority_t)osPriorityNormal,
+                                            .stack_size = 128 * 4};
 
 void StartM4DefaultTask(void *argument);
 
 void MX_FREERTOS_Init(void);
 
 void MX_FREERTOS_Init(void) {
-    setup();
-    // cm4_task_handle = osThreadNew(StartM4DefaultTask, NULL, &cm4_task_attributes);
+  // setup(); //<-this line
+  cm4_task_handle = osThreadNew(StartM4DefaultTask, NULL, &cm4_task_attributes);
 }
 
 void StartM4DefaultTask(void *argument) {
-    // GPIO_InitTypeDef GPIO_InitStructure;
-    // GPIO_InitStructure.Pin = LED_B_Pin;
-    // GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
-    // GPIO_InitStructure.Speed = GPIO_SPEED_LOW;
-    // HAL_GPIO_Init(LED_B_GPIO_Port, &GPIO_InitStructure);
-    // HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, 1);
-    // /* Infinite loop */
-    // while(1) {
-    //     osDelay(1000);
-    //     HAL_GPIO_TogglePin(LED_B_GPIO_Port, LED_B_Pin);
-    // }
-    loop();
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.Pin = LED_B_Pin;
+  GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStructure.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init(LED_B_GPIO_Port, &GPIO_InitStructure);
+  HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, 1);
+  /* Infinite loop */
+  while (1) {
+    osDelay(1000);
+    HAL_GPIO_TogglePin(LED_B_GPIO_Port, LED_B_Pin);
+  }
+  // loop(); //<-this line
 }
--- Makefile
+++ Makefile
@@ -52,8 +52,11 @@ clean:
 	@echo Deleting Completed
 	@echo Leave $(CURR_DIR)
 
-OBJ_M4 := $(shell $(find . -path "./CM7" -prune -o -print | grep "\.o$"))
-OBJ_M7 := $(shell $(find . -path "./CM4" -prune -o -print | grep "\.o$"))
+# OBJ_M4 := $(shell find  . -path ./CM7 -prune -o -name "*.o" -print -o -name "*4.ino" -print)
+# OBJ_M7 := $(shell find  . -path ./CM4 -prune -o -name "*.o" -print -o -name "*7.ino" -print)
+
+OBJ_M4 := $(shell find  . -path ./CM7 -prune -o -name "*.o" -print)
+OBJ_M7 := $(shell find  . -path ./CM4 -prune -o -name "*.o" -print)
 
 # This should work (?
 .PHONY: test_M4.elf
