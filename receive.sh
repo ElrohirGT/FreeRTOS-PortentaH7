edecf530d7dbc033317fe9feba0a1d4675f330ae
ElrohirGT <elrohirgt@gmail.com>
2026-02-19 20:24:52 -0600

    fix: .sh shebangs

--- receive.sh
+++ receive.sh
@@ -1,10 +1,10 @@
+#!/usr/bin/env bash
 
 # This file is a part of MRNIU/FreeRTOS-PortentaH7 (https://github.com/MRNIU/FreeRTOS-PortentaH7).
 #
 # receive.sh for MRNIU/FreeRTOS-PortentaH7.
 
 stty -f /dev/cu.usbmodem2003645000002 raw speed 9600 min 0 time 10
-while [ 1 -eq 1 ]
-do
-    cat /dev/cu.usbmodem2003645000002 >> "recv.log"
+while [ 1 -eq 1 ]; do
+	cat /dev/cu.usbmodem2003645000002 >>"recv.log"
 done
--- send_recv.sh
+++ send_recv.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 
 # This file is a part of MRNIU/FreeRTOS-PortentaH7 (https://github.com/MRNIU/FreeRTOS-PortentaH7).
 #
 # send_recv.sh for MRNIU/FreeRTOS-PortentaH7.
 
-echo 0 > /dev/cu.usbmodem2003645000002 | cat /dev/cu.usbmodem2003645000002
+echo 0 >/dev/cu.usbmodem2003645000002 | cat /dev/cu.usbmodem2003645000002
